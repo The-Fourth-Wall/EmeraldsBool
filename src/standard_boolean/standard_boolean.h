490dba9f9991fde91535e589705e194e89e00473
oblivious <thanos.papap42@gmail.com>
2024-08-22 23:37:46 +0300

    Integrated with EmeraldsPreprocessor.

--- src/standard_boolean/standard_boolean.h
+++ src/standard_boolean/standard_boolean.h
@@ -1,8 +1,10 @@
 #ifndef __STANDARD_BOOLEAN_H_
 #define __STANDARD_BOOLEAN_H_
 
+#include "../../libs/EmeraldsPreprocessor/export/EmeraldsPreprocessor.h"
+
 /** @param bool -> A 'big' enough size to hold both 1 and 0 **/
-#if __STDC_VERSION__ < 202000L
+#if PREPROCESSOR_C_VERSION < 2023
   #define bool  unsigned char
   #define true  1
   #define false 0
