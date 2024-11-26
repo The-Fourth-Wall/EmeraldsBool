666bb71a861345087c790261178c6cd4daee83ed
oblivious <thanos.papap42@gmail.com>
2024-11-26 14:57:47 +0200

    Updated license with special provision.

--- src/EmeraldsBool.h
+++ src/EmeraldsBool.h
@@ -1,6 +1,6 @@
 /**
  * A simplistic boolean data type.
- * Copyright (C) 2024 oblivious
+ * Copyright (C) 2021-2025 oblivious
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As a special exception, this library may be used in programs licensed
+ * under any terms.  Modifications to the library itself must be licensed
+ * under the GNU General Public License version 3, with the inclusion of
+ * this special exception, while modifications to programs using this
+ * library may continue to be licensed under any terms.  This exception
+ * does not impose any additional licensing requirements, modify or
+ * transform the licensing terms of programs using this library.
  */
 
 #ifndef __EMERALDSBOOL_H_
